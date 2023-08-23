dfc5e61863c39eb0081919965e885279575f42a1
Adelyn Flowers <adelyn.flowers@gmail.com>
2023-08-22 20:12:41 -0400

    set default font to arial

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <QtQuick>
 #include <QFontDatabase>
 #include <kaeLib.h>
-
+#include <QFont>
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
@@ -12,6 +12,8 @@ int main(int argc, char *argv[])
     KaeLib kaeLib;
     if (QFontDatabase::addApplicationFont(":/fonts/fontello.ttf") == -1)
         qWarning() << "Failed to load fontello.ttf";
+    QFont fon("Arial", 10);
+    app.setFont(fon);
     engine.rootContext()->setContextProperty("kaeLib", &kaeLib);
     QObject::connect(
         &engine, &QQmlApplicationEngine::objectCreationFailed,
