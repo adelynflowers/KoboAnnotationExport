153901567ec9d679dfb3d6e61d1349017ae08943
Adelyn Flowers <adelyn.flowers@gmail.com>
2023-08-25 19:53:26 -0400

    added custom sorting

--- src/BookModel/proxyModel.cpp
+++ src/BookModel/proxyModel.cpp
@@ -14,13 +14,18 @@ bool BookProxyModel::lessThan(const QModelIndex &left, const QModelIndex &right)
     auto rTitle{sourceModel()->data(right, BookModel::RoleNames::TitleRole).toString()};
     auto lDate{sourceModel()->data(left, BookModel::RoleNames::DateRole).toString()};
     auto rDate{sourceModel()->data(right, BookModel::RoleNames::DateRole).toString()};
-    if (lTitle == rTitle)
-    {
+    if (titleEnabled && dateEnabled) {
+        return lTitle == rTitle ? lDate < rDate : lTitle < rTitle;
+    }
+    else if (titleEnabled && !dateEnabled) {
+        return lTitle < rTitle;
+    }
+    else if (!titleEnabled && dateEnabled) {
         return lDate < rDate;
     }
-    else
-    {
-        return lTitle < rTitle;
+    else {
+        //TODO: Don't fail silently!
+        return true;
     }
 }
 bool BookProxyModel::filterAcceptsRow(int row, const QModelIndex &parent) const
@@ -28,4 +33,11 @@ bool BookProxyModel::filterAcceptsRow(int row, const QModelIndex &parent) const
     auto idx = sourceModel()->index(row, 0, parent);
     return sourceModel()->data(idx, BookModel::RoleNames::TextRole).toString().contains(filterRegularExpression()) ||
            sourceModel()->data(idx, BookModel::RoleNames::TitleRole).toString().contains(filterRegularExpression());
+}
+
+void BookProxyModel::customSort(bool useDate, bool useTitle, Qt::SortOrder order) {
+    this->dateEnabled = useDate;
+    this->titleEnabled = useTitle;
+    this->sort(0, order);
+
 }
--- src/BookModel/proxyModel.h
+++ src/BookModel/proxyModel.h
@@ -9,12 +9,16 @@ class BookProxyModel : public QSortFilterProxyModel
     Q_OBJECT
     QML_ELEMENT
 public:
-    BookProxyModel(QObject *parent = 0);
-    ~BookProxyModel();
+    explicit BookProxyModel(QObject *parent = nullptr);
+    ~BookProxyModel() override;
+    void customSort(bool dateEnabled, bool titleEnabled, Qt::SortOrder order = Qt::AscendingOrder);
 
 protected:
-    bool lessThan(const QModelIndex &source_left, const QModelIndex &source_right) const;
-    bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const;
+    bool lessThan(const QModelIndex &source_left, const QModelIndex &source_right) const override;
+    bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const override;
+
+    bool titleEnabled = true;
+    bool dateEnabled = true;
 };
 
 #endif // PROXYMODEL_H
