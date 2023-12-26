2dd22ff54993f6a1ca9751a7fed767e4946a36c4
Adelyn Flowers <adelyn.flowers@gmail.com>
2023-12-26 16:14:42 -0500

    added AppImage build flow

---
+++ linux/docker-build.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+sudo apt update; 
+sudo apt install -y \
+    libgl-dev \
+    libvulkan-dev \
+    curl \
+    zip \
+    unzip \
+    tar \
+    pkg-config \
+    wget \
+    libxcb-xkb-dev \
+    libxkbcommon-x11-dev \
+    libxkbcommon-dev \
+    libx11-xcb-dev \
+    libx11-xcb1 \
+    build-essential \
+    libxcb-util-dev \
+    libfontconfig1-dev \
+    libfreetype6-dev \
+    libx11-dev \
+    libx11-xcb-dev \
+    libxext-dev \
+    libxfixes-dev \
+    libxi-dev \
+    libxrender-dev \
+    libxcb1-dev \
+    libxcb-cursor-dev \
+    libxcb-glx0-dev \
+    libxcb-keysyms1-dev \
+    libxcb-image0-dev \
+    libxcb-shm0-dev \
+    libxcb-icccm4-dev \
+    libxcb-sync-dev \
+    libxcb-xfixes0-dev \
+    libxcb-shape0-dev \
+    libxcb-randr0-dev \
+    libxcb-render-util0-dev \
+    libxcb-util-dev \
+    libxcb-xinerama0-dev \
+    libxcb-xkb-dev \
+    libxkbcommon-dev \
+    libxkbcommon-x11-dev \
+    libfontconfig1-dev \
+    libfreetype6-dev \
+    libx11-dev \
+    libx11-xcb-dev \
+    libxext-dev \
+    libxfixes-dev \
+    libxi-dev \
+    libxrender-dev \
+    libxcb1-dev \
+    libxcb-cursor-dev \
+    libxcb-glx0-dev \
+    libxcb-keysyms1-dev \
+    libxcb-image0-dev \
+    libxcb-shm0-dev \
+    libxcb-icccm4-dev \
+    libxcb-sync-dev \
+    libxcb-xfixes0-dev \
+    libxcb-shape0-dev \
+    libxcb-randr0-dev \
+    libxcb-render-util0-dev \
+    libxcb-util-dev \
+    libxcb-xinerama0-dev \
+    libxcb-xkb-dev \
+    libxkbcommon-dev \
+    libxkbcommon-x11-dev \
+    file;
+sudo chmod -R 777 /home/user/project;
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20231206-1/linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+sed 's|AI\x02|\x00\x00\x00|g' -i linuxdeploy-x86_64.AppImage
+sed 's|AI\x02|\x00\x00\x00|g' -i linuxdeploy-plugin-qt-x86_64.AppImage
+chmod +x linuxdeploy-x86_64.AppImage
+chmod +x linuxdeploy-plugin-qt-x86_64.AppImage
+export APPIMAGE_EXTRACT_AND_RUN=1
+cd /home/user/project;
+./vcpkg.sh;
+qt-cmake . -G Ninja -B ./build-linux; cmake --build ./build-linux;
+export QML_SOURCES_PATHS=/home/user/project/build-linux
+mkdir deploy || true;
+~/linuxdeploy-x86_64.AppImage --plugin qt -e ./build-linux/kae --appdir ./build-linux/deploy -d ./linux/kae.desktop -i ./linux/kae.png --output appimage;
+mv kae*.AppImage ./deploy;
+rm -rf ./build-linux;
+rm -rf ./vcpkg;
+
--- wasmbuild.sh
+++
@@ -1,6 +0,0 @@
-rm -rf build-wasm;
-sudo apt update;
-sudo apt install -y build-essential curl zip unzip tar pkg-config;
-./vcpkg.sh;
-qt-cmake . -G Ninja -B ./build-wasm -DVCPKG_TARGET_TRIPLET=wasm32-emscripten; 
-cmake --build ./build-wasm;
